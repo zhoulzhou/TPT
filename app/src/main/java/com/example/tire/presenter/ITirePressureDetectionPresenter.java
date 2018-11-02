22c06773d78c74b86ed4a31a3f9715094707c449
ws <sohozhou@163.com>
2018-11-02 15:58:09 +0800

    presenter层OK

---
+++ app/src/main/java/com/example/tire/presenter/ITirePressureDetectionPresenter.java
@@ -0,0 +1,4 @@
+package com.example.tire.presenter;
+
+public interface ITirePressureDetectionPresenter {
+}
---
+++ app/src/main/java/com/example/tire/presenter/TirePressureDetectionPresenter.java
@@ -0,0 +1,4 @@
+package com.example.tire.presenter;
+
+public class TirePressureDetectionPresenter implements ITirePressureDetectionPresenter {
+}
