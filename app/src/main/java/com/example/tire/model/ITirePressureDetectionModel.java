8fc5a5267c81c6a063d81a8a642a25b15a186033
ws <sohozhou@163.com>
2018-11-05 10:57:25 +0800

    定义了Model层的接口，待具体实现

--- app/src/main/java/com/example/tire/model/ITirePressureDetectionModel.java
+++ app/src/main/java/com/example/tire/model/ITirePressureDetectionModel.java
@@ -1,4 +1,6 @@
 package com.example.tire.model;
 
 public interface ITirePressureDetectionModel {
+    float getTirePressureFromDB(int whichTire);
+    float getTireTemperatureFromDB(int whichTire);
 }
--- app/src/main/java/com/example/tire/model/TirePressureDetectionModel.java
+++ app/src/main/java/com/example/tire/model/TirePressureDetectionModel.java
@@ -1,4 +1,14 @@
 package com.example.tire.model;
 
 public class TirePressureDetectionModel implements ITirePressureDetectionModel {
+
+    @Override
+    public float getTirePressureFromDB(int whichTire) {
+        return 0;
+    }
+
+    @Override
+    public float getTireTemperatureFromDB(int whichTire) {
+        return 0;
+    }
 }
