364ac0e280f372a64b26ffe8d46c588f5f6f0252
ws <sohozhou@163.com>
2018-11-05 11:28:08 +0800

    实现了presneter层

--- app/src/main/java/com/example/tire/presenter/TirePressureDetectionPresenter.java
+++ app/src/main/java/com/example/tire/presenter/TirePressureDetectionPresenter.java
@@ -1,11 +1,15 @@
 package com.example.tire.presenter;
 
+import android.os.Handler;
+
+import com.example.tire.common.TireUtils;
 import com.example.tire.model.ITirePressureDetectionModel;
 import com.example.tire.view.ITirePressureDetectionView;
 
 public class TirePressureDetectionPresenter implements ITirePressureDetectionPresenter {
     private ITirePressureDetectionView mTirePressureDetectionView;
     private ITirePressureDetectionModel mTirePressureDetectionModel;
+    private Handler mHandler = new Handler();
 
     public TirePressureDetectionPresenter(ITirePressureDetectionView view, ITirePressureDetectionModel model){
         mTirePressureDetectionView = view;
@@ -14,21 +18,34 @@ public class TirePressureDetectionPresenter implements ITirePressureDetectionPre
 
     @Override
     public float getTirePressure(int whichTire) {
-        return 0;
+        return mTirePressureDetectionModel.getTirePressureFromDB(whichTire);
     }
 
     @Override
     public float getTireTemperature(int whichTire) {
-        return 0;
+        return mTirePressureDetectionModel.getTireTemperatureFromDB(whichTire);
     }
 
     @Override
     public void startTireDetection() {
-
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //应该Model层去获取数据，回调到presenter层
+                mTirePressureDetectionView.showTirePressureFL(getTirePressure(TireUtils.TIRE_FL),
+                        getTireTemperature(TireUtils.TIRE_FL));
+                mTirePressureDetectionView.showTirePressureFR(getTirePressure(TireUtils.TIRE_FR),
+                        getTireTemperature(TireUtils.TIRE_FR));
+                mTirePressureDetectionView.showTirePressureBL(getTirePressure(TireUtils.TIRE_BL),
+                        getTireTemperature(TireUtils.TIRE_BL));
+                mTirePressureDetectionView.showTirePressureBR(getTirePressure(TireUtils.TIRE_BR),
+                        getTireTemperature(TireUtils.TIRE_BR));
+            }
+        }, 1000);
     }
 
     @Override
     public void stopTireDetection() {
-
+        mHandler.removeCallbacksAndMessages(null);
     }
 }
