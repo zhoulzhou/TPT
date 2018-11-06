6d4c3a779f18ed4363d783152df13d06cb9eaa97
ws <sohozhou@163.com>
2018-11-06 13:58:03 +0800

    通过fragment显示UI以便适配Pad

--- app/src/main/java/com/example/tire/MainActivity.java
+++ app/src/main/java/com/example/tire/MainActivity.java
@@ -1,91 +1,25 @@
 package com.example.tire;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
 
-import com.example.tire.common.LogUtils;
-import com.example.tire.model.TirePressureDetectionModel;
-import com.example.tire.presenter.ITirePressureDetectionPresenter;
-import com.example.tire.presenter.TirePressureDetectionPresenter;
-import com.example.tire.view.ITirePressureDetectionView;
-import com.example.tire.view.TwoTextView;
+import com.example.tire.view.TirePressureDetectionFragment;
 
-public class MainActivity extends AppCompatActivity implements ITirePressureDetectionView{
-    private TwoTextView mTirePressureFLText, mTirePressureFRText;
-    private TwoTextView mTirePressureBLText, mTirePressureBRText;
-    private ImageView mTirePressureFL, mTirePressureFR;
-    private ImageView mTirePressureBL, mTirePressureBR;
-    private ITirePressureDetectionPresenter mTirePressureDetectionPresenter;
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTirePressureFLText = (TwoTextView) findViewById(R.id.tire_fl_text);
-        mTirePressureFRText = (TwoTextView) findViewById(R.id.tire_fr_text);
-        mTirePressureBLText = (TwoTextView) findViewById(R.id.tire_bl_text);
-        mTirePressureBRText = (TwoTextView) findViewById(R.id.tire_br_text);
-        mTirePressureFL = (ImageView) findViewById(R.id.tire_fl);
-        mTirePressureFR = (ImageView) findViewById(R.id.tire_fr);
-        mTirePressureBL = (ImageView) findViewById(R.id.tire_bl);
-        mTirePressureBR = (ImageView) findViewById(R.id.tire_br);
-
-        mTirePressureDetectionPresenter = new TirePressureDetectionPresenter(TirePressureDetectionModel.getInstance());
-        mTirePressureDetectionPresenter.attachView(this);
-        mTirePressureDetectionPresenter.startTireDetection();
-    }
-
-    @Override
-    public void showTirePressureFL(float pressure, float temperature) {
-        mTirePressureFLText.setValue(pressure,temperature);
-        if(isDangerous(pressure,temperature)){
-            mTirePressureFL.setImageResource(R.color.red);
-        }else{
-            mTirePressureFL.setImageResource(R.color.blue);
+        if(savedInstanceState == null) {
+            FragmentManager mFragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+            TirePressureDetectionFragment tirePressureDetectionFragment = new TirePressureDetectionFragment();
+            fragmentTransaction.add(R.id.fragment_layout, tirePressureDetectionFragment, "tire_pressure_detection");
+            fragmentTransaction.commit();
         }
-    }
 
-    @Override
-    public void showTirePressureFR(float pressure, float temperature) {
-        LogUtils.d("showTirePressureFR pressure= " + pressure);
-        mTirePressureFRText.setValue(pressure,temperature);
-        if(isDangerous(pressure,temperature)){
-            mTirePressureFR.setImageResource(R.color.red);
-        }else{
-            mTirePressureFR.setImageResource(R.color.blue);
-        }
-    }
-
-    @Override
-    public void showTirePressureBL(float pressure, float temperature) {
-        mTirePressureBLText.setValue(pressure,temperature);
-        if(isDangerous(pressure,temperature)){
-            mTirePressureBL.setImageResource(R.color.red);
-        }else{
-            mTirePressureBL.setImageResource(R.color.blue);
-        }
-    }
-
-    @Override
-    public void showTirePressureBR(float pressure, float temperature) {
-        mTirePressureBRText.setValue(pressure,temperature);
-        if(isDangerous(pressure,temperature)){
-            mTirePressureBR.setImageResource(R.color.red);
-        }else{
-            mTirePressureBR.setImageResource(R.color.blue);
-        }
-    }
-
-    private boolean isDangerous(float pressure, float temperature){
-        return pressure < 2 || pressure > 8 || temperature < 20 || temperature > 80;
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mTirePressureDetectionPresenter.stopTireDetection();
-        mTirePressureDetectionPresenter.detachView();
     }
 }
---
+++ app/src/main/java/com/example/tire/view/TirePressureDetectionFragment.java
@@ -0,0 +1,102 @@
+package com.example.tire.view;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.tire.R;
+import com.example.tire.common.LogUtils;
+import com.example.tire.model.TirePressureDetectionModel;
+import com.example.tire.presenter.ITirePressureDetectionPresenter;
+import com.example.tire.presenter.TirePressureDetectionPresenter;
+
+public class TirePressureDetectionFragment extends Fragment implements ITirePressureDetectionView {
+    private TwoTextView mTirePressureFLText, mTirePressureFRText;
+    private TwoTextView mTirePressureBLText, mTirePressureBRText;
+    private ImageView mTirePressureFL, mTirePressureFR;
+    private ImageView mTirePressureBL, mTirePressureBR;
+    private ITirePressureDetectionPresenter mTirePressureDetectionPresenter;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tire_pressure_detection_layout, container, false);
+        mTirePressureFLText = (TwoTextView) view.findViewById(R.id.tire_fl_text);
+        mTirePressureFRText = (TwoTextView) view.findViewById(R.id.tire_fr_text);
+        mTirePressureBLText = (TwoTextView) view.findViewById(R.id.tire_bl_text);
+        mTirePressureBRText = (TwoTextView) view.findViewById(R.id.tire_br_text);
+        mTirePressureFL = (ImageView) view.findViewById(R.id.tire_fl);
+        mTirePressureFR = (ImageView) view.findViewById(R.id.tire_fr);
+        mTirePressureBL = (ImageView) view.findViewById(R.id.tire_bl);
+        mTirePressureBR = (ImageView) view.findViewById(R.id.tire_br);
+
+        mTirePressureDetectionPresenter = new TirePressureDetectionPresenter(TirePressureDetectionModel.getInstance());
+        mTirePressureDetectionPresenter.attachView(this);
+        mTirePressureDetectionPresenter.startTireDetection();
+
+        return view;
+    }
+
+    @Override
+    public void showTirePressureFL(float pressure, float temperature) {
+        mTirePressureFLText.setValue(pressure,temperature);
+        if(isDangerous(pressure,temperature)){
+            mTirePressureFL.setImageResource(R.color.red);
+        }else{
+            mTirePressureFL.setImageResource(R.color.blue);
+        }
+    }
+
+    @Override
+    public void showTirePressureFR(float pressure, float temperature) {
+        LogUtils.d("showTirePressureFR pressure= " + pressure);
+        mTirePressureFRText.setValue(pressure,temperature);
+        if(isDangerous(pressure,temperature)){
+            mTirePressureFR.setImageResource(R.color.red);
+        }else{
+            mTirePressureFR.setImageResource(R.color.blue);
+        }
+    }
+
+    @Override
+    public void showTirePressureBL(float pressure, float temperature) {
+        mTirePressureBLText.setValue(pressure,temperature);
+        if(isDangerous(pressure,temperature)){
+            mTirePressureBL.setImageResource(R.color.red);
+        }else{
+            mTirePressureBL.setImageResource(R.color.blue);
+        }
+    }
+
+    @Override
+    public void showTirePressureBR(float pressure, float temperature) {
+        mTirePressureBRText.setValue(pressure,temperature);
+        if(isDangerous(pressure,temperature)){
+            mTirePressureBR.setImageResource(R.color.red);
+        }else{
+            mTirePressureBR.setImageResource(R.color.blue);
+        }
+    }
+
+    private boolean isDangerous(float pressure, float temperature){
+        return pressure < 2 || pressure > 8 || temperature < 20 || temperature > 80;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mTirePressureDetectionPresenter.stopTireDetection();
+        mTirePressureDetectionPresenter.detachView();
+    }
+}
