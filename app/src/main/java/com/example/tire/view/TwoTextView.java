81d71265f4010aa63fa3e2756efbb6c12e4ee1d5
ws <sohozhou@163.com>
2018-11-07 13:37:10 +0800

    去掉不用的资源

--- app/src/main/java/com/example/tire/view/TwoTextView.java
+++ app/src/main/java/com/example/tire/view/TwoTextView.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
