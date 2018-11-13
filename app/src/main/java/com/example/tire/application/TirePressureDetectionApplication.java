c796453d36d1bd1a2261b820c4e019ea7658d778
ws <sohozhou@163.com>
2018-11-13 15:18:44 +0800

    定义 application context

---
+++ app/src/main/java/com/example/tire/application/TirePressureDetectionApplication.java
@@ -0,0 +1,18 @@
+package com.example.tire.application;
+
+import android.app.Application;
+import android.content.Context;
+
+public class TirePressureDetectionApplication extends Application{
+    private static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        TirePressureDetectionApplication.mContext = getApplicationContext();
+    }
+
+    public static Context getContext(){
+        return TirePressureDetectionApplication.mContext;
+    }
+}
