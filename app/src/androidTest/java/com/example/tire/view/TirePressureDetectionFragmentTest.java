c82c82f9e09736718ccf11e74416d161755f3b61
ws <sohozhou@163.com>
2018-11-15 16:10:27 +0800

    增加单元测试 espresso 测试fragment

---
+++ app/src/androidTest/java/com/example/tire/view/TirePressureDetectionFragmentTest.java
@@ -0,0 +1,47 @@
+package com.example.tire.view;
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityTestCase;
+import android.test.ActivityUnitTestCase;
+
+import com.example.tire.R;
+import com.example.tire.application.TirePressureDetectionActivity;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class TirePressureDetectionFragmentTest {
+    @Rule
+    public ActivityTestRule<TirePressureDetectionActivity> mActivityRule =
+            new ActivityTestRule<>(TirePressureDetectionActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testFragment() throws Exception{
+        ViewInteraction car_bg = onView(withId(R.id.car_bg));
+        car_bg.check(doesNotExist());
+        car_bg.check(matches(isDisplayed()));
+    }
+}
