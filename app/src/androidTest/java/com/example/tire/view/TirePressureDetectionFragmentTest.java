956c2cefcd3f4115db67026b9a7acead50a6e5de
ws <sohozhou@163.com>
2018-11-15 16:13:06 +0800

    增加单元测试 espresso 测试fragment 测试点击  只是点击没效果  因为没有处理点击事件

--- app/src/androidTest/java/com/example/tire/view/TirePressureDetectionFragmentTest.java
+++ app/src/androidTest/java/com/example/tire/view/TirePressureDetectionFragmentTest.java
@@ -17,6 +17,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -43,5 +44,6 @@ public class TirePressureDetectionFragmentTest {
         ViewInteraction car_bg = onView(withId(R.id.car_bg));
         car_bg.check(doesNotExist());
         car_bg.check(matches(isDisplayed()));
+        car_bg.perform(click());
     }
 }
