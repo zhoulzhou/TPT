d8f17fad8828eed7ac092ba092c1c4251f24ca9d
ws <sohozhou@163.com>
2018-12-12 16:15:31 +0800

    增加个测试项目 后续用于小功能的测试 测试：通过string的name去查找资源对应的ID

---
+++ app/src/main/java/com/example/tire/test/TestMainActivity.java
@@ -0,0 +1,24 @@
+package com.example.tire.test;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class TestMainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        checkValue("Bodywork.getAutoType>69 DB>0","Bodywork.getAutoType");
+    }
+
+    int resId;
+    StringBuffer showBuffer = new StringBuffer();
+    private void checkValue(String source, String target){
+        if(source.contains(target)){
+            resId = getResources().getIdentifier(target.replace(".", "_").toLowerCase(), "string", "com.example.tire");
+            showBuffer.append(getResources().getString(resId) + "->" + source);
+        }
+
+        System.out.println("checkValue = " + showBuffer.toString());
+    }
+}
