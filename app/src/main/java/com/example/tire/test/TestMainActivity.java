82b270dd06064820f589ab141eb478caf9862dea
ws <sohozhou@163.com>
2018-12-13 17:13:40 +0800

    增加单元测试  在被测试的方法前面加test 具体使用要测试的方法：checkValue(),单元测试时为：testcheckValue();

--- app/src/main/java/com/example/tire/test/TestMainActivity.java
+++ app/src/main/java/com/example/tire/test/TestMainActivity.java
@@ -8,12 +8,12 @@ public class TestMainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        checkValue("Bodywork.getAutoType>69 DB>0","Bodywork.getAutoType");
+//        checkValue("Bodywork.getAutoType>69 DB>0","Bodywork.getAutoType");
     }
 
     int resId;
     StringBuffer showBuffer = new StringBuffer();
-    private void checkValue(String source, String target){
+    public void checkValue(String source, String target){
         if(source.contains(target)){
             resId = getResources().getIdentifier(target.replace(".", "_").toLowerCase(), "string", "com.example.tire");
             showBuffer.append(getResources().getString(resId) + "->" + source);
