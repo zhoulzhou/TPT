9f1314a62f8bccc337eb6a112e7678cfc2ed6f81
ws <sohozhou@163.com>
2019-01-04 16:08:33 +0800

    测试decodeStream 加载到80张出现OOM  达到最大内存192M

--- app/src/main/java/com/example/tire/frameanimation/FrameAnimationMainActivity.java
+++ app/src/main/java/com/example/tire/frameanimation/FrameAnimationMainActivity.java
@@ -171,7 +171,8 @@ public class FrameAnimationMainActivity extends AppCompatActivity {
 //        startAircondition();
 //        startyellow();
 //        testDrawable();
-        testBitmap();
+//        testBitmap();
+        testDecodeStream();
     }
 
     private void startyellow(){
@@ -242,12 +243,13 @@ public class FrameAnimationMainActivity extends AppCompatActivity {
         }
     }
 
-//    private void testDecodeStream(){
-//        bitmapArray = new Bitmap[number];
-//        for(int i=0; i<number; i++){
-//            LogUtils.d("测试第" + (i+1) + "张图片");
-//            bitmapArray[i] = BitmapFactory.decodeStream(getResources().openRawResource(+R.drawable.electric_wind_level_animation_00));
-//        }
-//    }
+    private void testDecodeStream(){
+        number = 1000;
+        bitmapArray = new Bitmap[number];
+        for(int i=0; i<number; i++){
+            LogUtils.d("测试第" + (i+1) + "张图片");
+            bitmapArray[i] = BitmapFactory.decodeStream(getResources().openRawResource(+R.drawable.electric_wind_level_animation_00));
+        }
+    }
 
 }
