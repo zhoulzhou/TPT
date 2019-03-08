80022727249744a9f9f4e6569ae825e6693d5a04
ws <sohozhou@163.com>
2019-03-08 13:46:08 +0800

    开关控件

---
+++ app/src/main/java/com/example/tire/selfview/SlideSwitch.java
@@ -0,0 +1,16 @@
+package com.example.tire.selfview;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class SlideSwitch extends View {
+    public SlideSwitch(Context context) {
+        super(context);
+    }
+
+    public SlideSwitch(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
