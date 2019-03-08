9e45d7d0dacdc5742d36fc3db172f3c384af6cee
ws <sohozhou@163.com>
2019-03-08 13:51:13 +0800

    开关控件 完成Measure

--- app/src/main/java/com/example/tire/selfview/SlideSwitch.java
+++ app/src/main/java/com/example/tire/selfview/SlideSwitch.java
@@ -1,11 +1,18 @@
 package com.example.tire.selfview;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class SlideSwitch extends View {
+    private Bitmap mBackgroundBitmap;
+    private Bitmap mSlideBitmap;
+
+    private float mViewWidth, mViewHeight;
+
     public SlideSwitch(Context context) {
         super(context);
     }
@@ -13,4 +20,30 @@ public class SlideSwitch extends View {
     public SlideSwitch(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mViewHeight = measureSize(heightMeasureSpec);
+        mViewWidth = measureSize(widthMeasureSpec);
+        setMeasuredDimension((int) mViewWidth, (int) mViewHeight);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    private int measureSize(int measureSpec){
+        int realSize = 10;
+        int size = MeasureSpec.getSize(measureSpec);
+        int mode = MeasureSpec.getMode(measureSpec);
+
+        if(mode == MeasureSpec.EXACTLY){
+            realSize = size;
+        }
+
+        return realSize;
+    }
+
 }
