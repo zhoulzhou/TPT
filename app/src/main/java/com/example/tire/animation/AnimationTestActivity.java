349be1ba0014885005cdff73364e4df652785a61
ws <sohozhou@163.com>
2019-03-26 09:34:51 +0800

    valueanimator  改变长宽，及Layout的值

---
+++ app/src/main/java/com/example/tire/animation/AnimationTestActivity.java
@@ -0,0 +1,58 @@
+package com.example.tire.animation;
+
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.BounceInterpolator;
+
+import com.example.tire.R;
+import com.example.tire.common.LogUtils;
+
+public class AnimationTestActivity extends AppCompatActivity {
+    private View mView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.animation_test_layout);
+        mView = findViewById(R.id.an_view);
+//        an1();
+        an2();
+    }
+
+    private void an1(){
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(20,600);
+        valueAnimator.setDuration(3000);
+        valueAnimator.setRepeatCount(3);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int curValue = (int) animation.getAnimatedValue();
+                LogUtils.d("curValue= " + curValue);
+                ViewGroup.LayoutParams layoutParams = mView.getLayoutParams();
+                layoutParams.width = layoutParams.height = curValue;
+                mView.setLayoutParams(layoutParams);
+            }
+        });
+        valueAnimator.setInterpolator(new BounceInterpolator());
+        valueAnimator.start();
+    }
+
+    private void an2(){
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(0,400);
+        valueAnimator.setDuration(3000);
+        valueAnimator.setRepeatCount(1);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int curValue = (int) animation.getAnimatedValue();
+                LogUtils.d("curValue= " + curValue);
+                mView.layout(curValue,curValue,curValue+mView.getWidth(),curValue+mView.getHeight());
+            }
+        });
+        valueAnimator.start();
+    }
+}
