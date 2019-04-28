2a75d24dae980153188215e635464d88704a47f2
ws <sohozhou@163.com>
2019-04-28 11:42:05 +0800

    draw内存测试

--- app/src/main/java/com/example/tire/selfview/SelfViewActivity.java
+++ app/src/main/java/com/example/tire/selfview/SelfViewActivity.java
@@ -7,9 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import com.example.tire.R;
 
 public class SelfViewActivity extends AppCompatActivity {
+    private double[] mFuelPointValue = {};
+    private double[] mElecPointValue = {};
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.self_view_activity_layout);
+        LineChartView lineChartView = (LineChartView) findViewById(R.id.line_chart);
+        lineChartView.setData(mFuelPointValue,mElecPointValue,2);
     }
 }
